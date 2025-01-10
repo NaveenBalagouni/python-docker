95ea80c38a98191170d2e57714b56fa548e8a871
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2025-01-10 19:17:28 +0530

    Update app.py

--- python-docker-app/python-app/app.py
+++ python-docker-app/python-app/app.py
@@ -1,2 +1,10 @@
 # python-app/app.py
-print("Hello, World naveen sample nginx image")
+print("Hello, World naveen sample nginx image
+1. create a Github repo - put yaml all files same repo
+2. Put some python Script inside in image {print Hello world}
+3. create a Dockerfile
+4. create a image and pushed to Docker registry
+5. Create a workflow  that automates the above four steps
+6) create webhook es {eventsource}, sensor with workflow from step 5
+create a webhook repo to
+7 make change in changed in python script")
