c28696f9a46691256e9bda28b547b842287080d6
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2025-01-11 15:58:08 +0530

    Update app.py

--- python-docker-app/python-app/app.py
+++ python-docker-app/python-app/app.py
@@ -1,6 +1,6 @@
 print("Hello, World naveen sample nginx image")
 print("1. Create a Github repo - put YAML and all files in the same repo")
-print("2. Put a Python script inside an image that prints 'Hello World'")
+print("2. Put a Python script inside an image that prints 'Hello World '")
 print("3. Create a Dockerfile")
 print("4. Create an image and push it to Docker registry")
 print("5. Create a workflow that automates the above four steps")
